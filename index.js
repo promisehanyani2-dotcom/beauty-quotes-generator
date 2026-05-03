02b0e6231a34f4022f59ebc5d1309a6ecdac8993
promisehanyani2-dotcom <promisehanyani2@gmail.com>
2026-05-03 13:26:31 +0200

    Added the AI API integration

--- index.js
+++ index.js
@@ -1,12 +1,27 @@
+function showQuote(response) {
+  (console.log("quote generated"),
+    new Typewriter("#quote", {
+      strings: response.data.answer,
+      autoStart: true,
+      delay: 25,
+      cursor: "",
+    }));
+}
+
 function generateQuote(event) {
   event.preventDefault();
 
-  new Typewriter("#quote", {
-    strings: "Wear your scars like stardust",
-    autoStart: true,
-    delay: 25,
-    cursor: "",
-  });
+  let quoteElement = document.querySelector("#input-instructions");
+  let apiKey = "e5f2b33t5a4924f02bo250f1a930212e";
+  let prompt = `Generate a beauty quote about ${quoteElement.value}`;
+  let context = "The quote should be short, inspiring and uplifting.";
+  let apiUrl = `https://api.shecodes.io/ai/v1/generate?prompt=${prompt}&context=${context}&key=${apiKey}`;
+
+  console.log("Generating quote...");
+  console.log(`context: ${context}`);
+  console.log(`prompt: ${prompt}`);
+
+  axios.get(apiUrl).then(showQuote);
 }
 
 let quoteElement = document.querySelector("#quote-generator-form");
